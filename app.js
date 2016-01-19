a98234a49dbc956ff624c354ebbe72ff8dc52a51
Evan Tatum <evantatum@hotmail.com>
2016-01-18 16:02:54 -0800

    good work done today

--- app.js
+++ app.js
@@ -1,37 +1,64 @@
+// window.onload = choosePic1;
+//window.onload = choosePic2;
+//window.onload = choosePic3;
+
+
+var imagesArray = ["bagWeek3.jpg ", "banana.jpg ", "boots.jpg", "chair.jpg", "cthulhu.jpg", "dragon.jpg", "pen.jpg", "scissors.jpg", "shark.jpg", "sweep.png", "unicorn.jpg", "usb.gif", "water-can.jpg", "wine-glass.jpg"];
+
+function choosePic1() {
+     var randomNum = Math.floor(Math.random() * imagesArray.length);
+     document.getElementById("firstImage").src= 'week-3Assets/' + imagesArray[randomNum];
+   }
+
+   function choosePic2() {
+        var randomNum = Math.floor(Math.random() * imagesArray.length);
+        document.getElementById("secondImage").src= 'week-3Assets/' + imagesArray[randomNum];
+      }
+
+      function choosePic3() {
+           var randomNum = Math.floor(Math.random() * imagesArray.length);
+           document.getElementById("thirdImage").src= 'week-3Assets/' + imagesArray[randomNum];
+         }
+
+         choosePic3();
+         choosePic2();
+         choosePic1();
 
 
 
 
-var first = document.getElementById('first');
-var second = document.getElementById('second');
-var third = document.getElementById('third');
-
-var firstClicks = 0;
-var secondClicks = 0;
-var thirdClicks = 0;
 
 
 
-first.addEventListener('click', clickOnFirst);
 
-function clickOnFirst() {
-  firstClicks += 1;
-  first.textContent =('First was clicked ' + firstClicks + 'times')
-  if ( firstClicks % 3 === 0){
-    first.textContent = 'I AM DIVISABLE BY 3!';
-  }
-}
 
-second.addEventListener('click', clickOnFirst);
 
-function clickOnFirst() {
-  firstClicks += 1;
-  second.textContent =('Second was clicked ' + firstClicks + 'times')
-}
-
-third.addEventListiner('click', clickOnFirst);
-
-function clickOnFirst() {
-  firstClicks += 1;
-  third.textContent =('Third was clicked ' + firstClicks + 'times')
-}
+// var first = document.getElementById('image1');
+// var second = document.getElementById('image2');
+// var third = document.getElementById('image3');
+//
+// first.addEventListener('click', handleClickOnFirst);
+// second.addEventListener('click', handleClickOnSecond);
+// third.addEventListener('click', handleClickOnThird);
+//
+// var firstClicks = 0;
+// var secondClicks = 0;
+// var thirdClicks = 0;
+//
+// function handleClickOnFirst() {
+//   firstClicks++;
+//   first.textContent = 'First was clicked ' + firstClicks + ' times';
+//   if (firstClicks % 3 === 0) {
+//     first.textContent = 'I AM DIVISIBLE BY 3!';
+//   }
+// }
+//
+// function handleClickOnSecond() {
+//   secondClicks += 1;
+//   second.textContent = 'Second was clicked ' + secondClicks + ' times';
+// }
+//
+// function handleClickOnThird() {
+//   thirdClicks += 1;
+//   third.textContent = 'Third was clicked ' + thirdClicks + ' times';
+// }
